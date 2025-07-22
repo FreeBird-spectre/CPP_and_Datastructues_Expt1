25551a1aae6278bfc9c2d8ac55869da7ca9d2300
FreeBird-spectre <sethabir699@gmail.com>
2025-07-22 18:56:36 +0530

    Update Calculator.cpp
    EXP 1B

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,1 +1,32 @@
+// Abir Seth
+// PRN-24070123003
+// Entc A1
+#include <iostream>
+using namespace std;
+int main() {
+ float num1,num2,multiplication,division ,subtraction,addition;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    addition=num1+num2;
+    subtraction=num1-num2;
+    multiplication=num1*num2;
+    division=num1/num2;
+    cout << "addition of given numbers:"<<addition<<endl; 
+    cout << "subtraction of given numbers:"<<subtraction<<endl;
+    cout << "multiplication of given numbers:"<<multiplication<<endl;
+    cout << "division of given numbers:"<<division<<endl;
 
+    return 0;
+}
+
+Enter first number: 7
+Enter second number: 8
+addition of given numbers:15
+subtraction of given numbers:-1
+multiplication of given numbers:56
+division of given numbers:0.875
+
+
+=== Code Execution Successful ===
